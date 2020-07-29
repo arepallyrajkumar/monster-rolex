0111112c404b18003276fa272f8e9e7c4da58a2c
Raja Kumar <rajak@wmn.local>
2020-07-29 15:42:39 -0500

    Updates

---
+++ precache-manifest.adbc778a843a1ad094c012f91341f6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b15fecf40640d1caddf8ea55c4c264",
+    "url": "/monster-rolex/index.html"
+  },
+  {
+    "revision": "1909ae63e424bc4f83a5",
+    "url": "/monster-rolex/static/css/main.9d04accf.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monster-rolex/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolex/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1909ae63e424bc4f83a5",
+    "url": "/monster-rolex/static/js/main.d03cd2fc.chunk.js"
+  },
+  {
+    "revision": "24bf591315e182452ac1",
+    "url": "/monster-rolex/static/js/runtime-main.e5f26da6.js"
+  }
+]);
